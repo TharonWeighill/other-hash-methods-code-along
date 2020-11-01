0bdc56a41dad62edd66b4818175cf7deb002d217
tharon <tharonweighill@gmail.com>
2020-11-01 14:34:36 -0700

    commit tree 3c57b4821e4f8e7c2d825548897ee1eafafe60cc

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,3 +1,4 @@
+
 require 'pry'
 
 #This is the groceries hash we'll be passing in to the get_the_keys method
@@ -9,5 +10,6 @@ require 'pry'
 # }
 
 def get_the_keys(groceries)
-  #code your solution here!
+  groceries.keys
+  [:dairy, :vegetable, :meat, :grains]
 end
